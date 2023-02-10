0f0f0dd2785dcded69530ee2fea99be169a1c97c
Pavan Parashuram <pavanparashuramdevang@yahoo.com>
2023-02-10 13:09:41 +0530

    first python commit

---
+++ python_language/basic_python/basic.py
@@ -0,0 +1,68 @@
+"""
+python is very easy to learn language 
+the syntax is similer to normal english
+but the problem is with speed of python and some people don't like the syntax 
+like me i also like the curly braces syntax more than python as I worked a lot in c and c inspired languages
+for basic it feels it's the best in syntax
+but as we start to write complex code base it feels cluttered
+for example for loop we can make it to iterate over any iterator which is good but as I said its depend
+on preference and taste of the people
+
+python is basically used for data science and AI which has every progarammer facing interface written in python
+as I said all AI framework have interface written in python but to under the hood most 
+still uses the C++ because of its execution efficiency
+
+the good thing of learning python is, it can be used to write anything, build anything 
+very easy to get started 
+very good community
+there always a tool to do anything like we can write an entire website from frontend, backend and everything
+only in python without using javascript
+
+it is possible, is it the best it's upto you
+
+its a object oriented language but at begining no one uses the oop concepts which is ok as 
+you can transition from language like c its very convinient 
+as you program in structured manner in python also at begining
+but in pytho everything is object, it doesn't enforce it on developer like JAVA in which
+
+you must write in object oriented way which results in very sophisticated and reusable codebase of 
+enterprices 
+
+I'm not discouraging, what I'm saying is if you want to enter into an enterprice JAVA is still the best 
+if you want to go on front end web development javascript with react/angular (as most used in industry )
+
+if you want to enter into data science, AI which are the hot topic in the silicon vally now
+python is must 
+
+choos wisely as i think you can learn all the languages but you think in one to two language
+thats also true for computer languages you think of solving problem in one language
+
+"""
+
+#you can use # to comment a line 
+#or you can use doc string i.e """ string inside three quotes ,to document
+
+#indentation is very important in python and use editor which converts tab to space is preferred
+#as python doesn't allow mix and match of space and tab
+
+
+# for i in range(10):
+#     for j in range(2):
+#         print("this is wrong indented code")
+
+
+#the above is mix and match of tab and space the code works I think editor just replaced tab to 4 spaces
+#automaticaly, not all editor do the same i.e notepad in windows so use an editor 
+
+
+
+
+print("hello world")
+
+# decleration of explicit type is not required as python Interpreter assumes the specific type depending
+# on the context i.e if and integer is not able to fit then it automatically converted to long
+# float to double etc
+
+#you can check type by printing type(object) on to screen example
+
+print(type(3))
